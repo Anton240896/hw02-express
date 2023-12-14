ff5461a6ce4a65a3a227cbfa94585b00073f9b50
Anton240896 <antonalipov791@gmail.com>
2023-12-14 15:59:46 +0200

    add

--- controllers/contact-controller.js
+++ controllers/contact-controller.js
@@ -1,5 +1,12 @@
 import * as contactsService from "../models/contacts.js";
 import { HttpError } from "../helpers/index.js";
+import Joi from "joi";
+
+const contactAddSchema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().required(),
+  phone: Joi.string().required(),
+});
 
 const getAll = async (req, res, next) => {
   try {
@@ -27,7 +34,7 @@ const getById = async (req, res, next) => {
 
 const add = async (req, res, next) => {
   try {
-    // console.log(req.body);
+    const { error } = contactAddSchema.validate(req.body);
     const result = await contactsService.addContact(req, body);
     res.status(201).json(result);
   } catch (error) {
