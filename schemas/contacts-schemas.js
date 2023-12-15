c47baf9f75ba00ff0e95fc0e01ecfc8b09a1cb1d
Anton240896 <antonalipov791@gmail.com>
2023-12-15 20:09:29 +0200

    Update contacts-schemas.js

--- schemas/contacts-schemas.js
+++ schemas/contacts-schemas.js
@@ -1,19 +1,13 @@
 import Joi from "joi";
 
 export const contactAddSchema = Joi.object({
-  name: Joi.string().required().messages({
-    "any.required": `"name" must be exist`,
-  }),
-  email: Joi.string().required().messages({
-    "any.required": `"email" must be exist"`,
-  }),
-  phone: Joi.string().required().messages({
-    "any.required": `"number" must be exist`,
-  }),
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
 });
 
 export const contactUpdateSchema = Joi.object({
-  name: Joi.string().required().messages({
-    "any.required": `"name" must be exist`,
-  }),
-});
+  name: Joi.string(),
+  email: Joi.string().email(),
+  phone: Joi.string(),
+}).or("name", "email", "phone");
